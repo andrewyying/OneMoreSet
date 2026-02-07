dcc900936132f6329979b386d0b24f2f71f578d9
Andrew Ying <69857969+andrewyying@users.noreply.github.com>
2026-02-06 22:43:56 -0600

    add congrats page

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -1,11 +1,12 @@
-export type RootStackParamList = {
-  MainTabs: undefined;
-  ScheduleEditor: { scheduleId: string };
-  Player: { scheduleId: string; startWithCountdown?: boolean };
-};
+import { NavigatorScreenParams } from '@react-navigation/native';
 
 export type MainTabParamList = {
   ScheduleList: undefined;
   Calendar: undefined;
 };
 
+export type RootStackParamList = {
+  MainTabs: NavigatorScreenParams<MainTabParamList> | undefined;
+  ScheduleEditor: { scheduleId: string };
+  Player: { scheduleId: string; startWithCountdown?: boolean };
+};
